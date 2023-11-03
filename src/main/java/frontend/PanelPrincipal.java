3c473672a8113878bb9c6d10aec64688bba6709f
gllrmoliva <gllrmoliva42@gmail.com>
2023-11-03 13:47:37 -0300

    Actualización documentación Panel Pricipal

--- src/main/java/frontend/PanelPrincipal.java
+++ src/main/java/frontend/PanelPrincipal.java
@@ -2,10 +2,18 @@ package frontend;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Es un panel que posee TODAS las vistas del programa (Vista de comprador y expendedor).
+ */
 public class PanelPrincipal extends JPanel {
     private static PanelComprador com;
     private static PanelExpendedor exp;
     private GridLayout grid;
+
+    /**
+     * Método constructor de PanelPrincipal. Los paneles Constructor y Expendedor son creados dentro de este.
+     * Estas a su vez, son controladas por la Clase Controller.
+     */
     public PanelPrincipal () {
         Controller.setPanelPrincipal(this);
         grid = new GridLayout(1,2);
